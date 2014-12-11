34d37b64bf5cf96e9949c470c6e864503a5c718e
guyreuven <guyreuven88@gmail.com>
2014-12-11 13:12:47 +0200

    commit all

--- src/Firewall.java
+++ src/Firewall.java
@@ -50,14 +50,17 @@ class SerialQueueFirewall {
     // allocate and initialize bank of numSources Lamport queues
     // each with depth queueDepth
     // they should throw FullException and EmptyException upon those conditions
-    
+    LamportsQueue<Packet>[] lamportQbank = new LamportsQueue[numSources];
+    for(LamportsQueue<Packet> lQ : lamportQbank)
+    {
+    	lQ = new LamportsQueue<Packet>(queueDepth);
+    }
 
     // Create a SerialQueuePackerWorker workerData 
     // as SerialPackerWorker, but be sure to Pass the lamport queues
-    SerialQueuePacketWorker workerData = new SerialQueuePacketWorker(done, pkt, uniformFlag, queueDepth); //TODO Change arguments....
+    SerialQueuePacketWorker workerData = new SerialQueuePacketWorker(done, pkt, uniformFlag, queueDepth, lamportQbank); 
     
     // The rest of the code looks as in Serial Firewall
-    
     Thread workerThread = new Thread(workerData);
     
     workerThread.start();
@@ -91,38 +94,80 @@ class ParallelFirewall {
     StopWatch timer = new StopWatch();
     PacketSource pkt = new PacketSource(mean, numSources, experimentNumber);
     
-    // ...
+   
     // Allocate and initialize bank of Lamport queues, as in SerialQueueFirewall
-    // ...
+    LamportsQueue<Packet>[] lamportQbank = new LamportsQueue[numSources];
+    for(LamportsQueue<Packet> lQ : lamportQbank)
+    {
+    	lQ = new LamportsQueue<Packet>(queueDepth);
+    }
+
     // Allocate and initialize any signals used to marshal threads (eg. done signals)
-    // ...
+    PaddedPrimitiveNonVolatile<Boolean>[] doneArr = new PaddedPrimitiveNonVolatile[numSources+2];
+    for(PaddedPrimitiveNonVolatile<Boolean> doneSignal:doneArr)
+    {
+    	doneSignal = new PaddedPrimitiveNonVolatile<Boolean>(false);
+    }
+
     // Allocate and initialize a Dispatcher class implementing Runnable
     // and a corresponding Dispatcher Thread
-    // ...
+    Dispatcher dispatcher = new Dispatcher(doneArr[0], lamportQbank, numSources, uniformFlag, pkt); 
+    Thread dispatcherThread = new Thread(dispatcher);
+    
     // Allocate and initialize an array of Worker classes (ParallelPacketWorker), 
     // implementing Runnable and the corresponding Worker Threads
-    // ...
+    Thread[] parallelPacketWorkerThreads = new Thread[numSources];
+    for(int i=1; i < (numSources+1); i++)
+    {
+    	ParallelPacketWorker parallelPacketWorker = new ParallelPacketWorker(doneArr[i], lamportQbank[i-1]);
+        parallelPacketWorkerThreads[i-1] = new Thread(parallelPacketWorker);
+    }
+
+    
     // Call start() for each worker
-    // ...
+    for(Thread tread : parallelPacketWorkerThreads)
+    {
+    	tread.start();
+    }
+
     timer.startTimer();
-    // ...
+    
     // Call start() for the Dispatcher thread
-    // ...
+    dispatcherThread.start();
     try {
       Thread.sleep(numMilliseconds);
     } catch (InterruptedException ignore) {;}
-    // 
-    // assert signals to stop Dispatcher - remember, Dispatcher needs to deliver an 
+    
+    // assert signals to stop Dispatcher - remember, Dispatcher needs to deliver an
     // equal number of packets from each source
-    //
+    doneArr[0].value = true;
     // call .join() on Dispatcher
-    //
+    try {
+		dispatcherThread.join();
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
     // assert signals to stop Workers - they are responsible for leaving the queues
     // empty - use whatever protocol you like, but one easy one is to have each
     // worker verify that it's corresponding queue is empty after it observes the
     // done signal set to true
-    //
+    for(int i=1; i < (numSources+1); i++)
+    {
+    	doneArr[i].value = true;
+    }
+    
+    
     // call .join() for each Worker
+    for(Thread tread : parallelPacketWorkerThreads)
+    {
+    	try {
+			tread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
     timer.stopTimer();
     // Output the statistics
   }
--- src/PacketWorker.java
+++ src/PacketWorker.java
@@ -1,84 +1,182 @@
 public interface PacketWorker extends Runnable {
-  public void run();
+	public void run();
 }
 
 class SerialPacketWorker implements PacketWorker {
-  PaddedPrimitiveNonVolatile<Boolean> done;
-  final PacketSource pkt;
-  final Fingerprint residue = new Fingerprint();
-  long fingerprint = 0;
-  long totalPackets = 0;
-  final int numSources;
-  final boolean uniformBool;
-  public SerialPacketWorker(
-    PaddedPrimitiveNonVolatile<Boolean> done, 
-    PacketSource pkt,
-    boolean uniformBool,
-    int numSources) {
-    this.done = done;
-    this.pkt = pkt;
-    this.uniformBool = uniformBool;
-    this.numSources = numSources;
-  }
-  
-  public void run() {
-    Packet tmp;
-    while( !done.value ) {
-      for( int i = 0; i < numSources; i++ ) {
-        if( uniformBool )
-          tmp = pkt.getUniformPacket(i);
-        else
-          tmp = pkt.getExponentialPacket(i);
-        totalPackets++;
-        fingerprint += residue.getFingerprint(tmp.iterations, tmp.seed);        
-      }
-    }
-  }  
+	PaddedPrimitiveNonVolatile<Boolean> done;
+	final PacketSource pkt;
+	final Fingerprint residue = new Fingerprint();
+	long fingerprint = 0;
+	long totalPackets = 0;
+	final int numSources;
+	final boolean uniformBool;
+	public SerialPacketWorker(
+			PaddedPrimitiveNonVolatile<Boolean> done, 
+			PacketSource pkt,
+			boolean uniformBool,
+			int numSources) {
+		this.done = done;
+		this.pkt = pkt;
+		this.uniformBool = uniformBool;
+		this.numSources = numSources;
+	}
+
+	public void run() {
+		Packet tmp;
+		while( !done.value ) {
+			for( int i = 0; i < numSources; i++ ) {
+				if( uniformBool )
+					tmp = pkt.getUniformPacket(i);
+				else
+					tmp = pkt.getExponentialPacket(i);
+				totalPackets++;
+				fingerprint += residue.getFingerprint(tmp.iterations, tmp.seed);        
+			}
+		}
+	}  
 }
 
 class SerialQueuePacketWorker implements PacketWorker {
-  PaddedPrimitiveNonVolatile<Boolean> done;
-  final PacketSource pkt;
-  final Fingerprint residue = new Fingerprint();
-  long fingerprint = 0;
-  long totalPackets = 0;
-  final int numSources;
-  final boolean uniformBool;
-  // Also all lamport queues.
-  public SerialQueuePacketWorker(
-    PaddedPrimitiveNonVolatile<Boolean> done, 
-    PacketSource pkt,
-    boolean uniformBool,
-    int numSources) {
-    this.done = done;
-    this.pkt = pkt;
-    this.uniformBool = uniformBool;
-    this.numSources = numSources;
-  }
-  
-  public void run() {
-    Packet tmp;
-    while( !done.value ) {
-      for( int i = 0; i < numSources; i++ ) {
-        if( uniformBool )
-          tmp = pkt.getUniformPacket(i);
-        else
-          tmp = pkt.getExponentialPacket(i);
-	// try {
-          // ...
-          // enqueue tmp in the ith Lamport queue
-          // ...
-        // } catch (FullException e) {;}
-        // try {
-          // ...
-          // dequeue the next packet from the ith Lamport queue into tmp
-          // ...
-        //} catch (EmptyException e) {;}
-        totalPackets++;
-        fingerprint += residue.getFingerprint(tmp.iterations, tmp.seed);        
-      }
-    }
-  }  
+	PaddedPrimitiveNonVolatile<Boolean> done;
+	final PacketSource pkt;
+	final Fingerprint residue = new Fingerprint();
+	long fingerprint = 0;
+	long totalPackets = 0;
+	final int numSources;
+	final boolean uniformBool;
+	final LamportsQueue<Packet>[] queueBank;
+
+	public SerialQueuePacketWorker(
+			PaddedPrimitiveNonVolatile<Boolean> done, 
+			PacketSource pkt,
+			boolean uniformBool,
+			int numSources,
+			LamportsQueue<Packet>[] queueBank) {
+		this.done = done;
+		this.pkt = pkt;
+		this.uniformBool = uniformBool;
+		this.numSources = numSources;
+		this.queueBank = queueBank;
+	}
+
+	public void run() {
+		Packet tmp;
+		while( !done.value ) {
+			for( int i = 0; i < numSources; i++ ) {
+				if( uniformBool )
+					tmp = pkt.getUniformPacket(i);
+				else
+					tmp = pkt.getExponentialPacket(i);
+				try {
+					//enqueue tmp in the ith Lamport queue
+					this.queueBank[i].enq(tmp);
+				} catch (FullException e) {;}
+				try {
+					//dequeue the next packet from the ith Lamport queue into tmp
+					tmp = this.queueBank[i].deq();
+				} catch (EmptyException e) {;}
+				totalPackets++;
+				fingerprint += residue.getFingerprint(tmp.iterations, tmp.seed);        
+			}
+		}
+	}  
 }
 
-// class ParallelPacketWorker implements PacketWorker...
+class ParallelPacketWorker implements PacketWorker {
+
+	PaddedPrimitiveNonVolatile<Boolean> done;
+	final Fingerprint residue = new Fingerprint();
+	long fingerprint = 0;
+	long numOfPackets = 0;
+	final LamportsQueue<Packet> lamportQueue;
+
+	public ParallelPacketWorker(PaddedPrimitiveNonVolatile<Boolean> done, LamportsQueue<Packet> lamportQueue) {
+		this.done = done;
+		this.lamportQueue = lamportQueue;
+	}
+
+	public void run() {
+		Packet tmp;
+		while( !done.value || !(lamportQueue.head == lamportQueue.tail)) {
+			try {
+				//dequeue the next packet from the ith Lamport queue into tmp
+				tmp = this.lamportQueue.deq();
+				numOfPackets++;
+				fingerprint += residue.getFingerprint(tmp.iterations, tmp.seed); 
+			} catch (EmptyException e) {
+				if(done.value)
+				{
+					break;
+				}
+			}
+		}
+	}  
+
+}
+
+class Dispatcher implements Runnable {
+
+	PaddedPrimitiveNonVolatile<Boolean> done;
+	final LamportsQueue<Packet>[] lamportQueueBank;
+	final PacketSource pkt;
+	final int numSources;
+	final boolean uniformBool;
+	int[] pktCounter;
+
+	public Dispatcher(PaddedPrimitiveNonVolatile<Boolean> done,
+			LamportsQueue<Packet>[] lamportQueueBank,
+			int numSources,
+			boolean uniformBool,
+			PacketSource pkt) {
+		this.done = done;
+		this.lamportQueueBank = lamportQueueBank;
+		this.numSources = numSources;
+		this.uniformBool = uniformBool;
+		this.pkt = pkt;
+		this.pktCounter = new int[numSources];
+	}
+
+	public void run() {
+		Packet tmp;
+		int maxPacketNum;
+		while( !done.value) {
+			for( int i = 0; i < numSources; i++ ) {
+				if( uniformBool )
+					tmp = pkt.getUniformPacket(i);
+				else
+					tmp = pkt.getExponentialPacket(i);
+				try {
+					//enqueue tmp in the ith Lamport queue
+					this.lamportQueueBank[i].enq(tmp);
+					this.pktCounter[i]++;
+				} catch (FullException e) {;}
+			}
+		}
+		
+		//calculate the maximum packet number from packet counter.
+		maxPacketNum = 0;
+		for(int pktNum:this.pktCounter)
+		{
+			if(pktNum > maxPacketNum)
+			{
+				maxPacketNum = pktNum;
+			}
+		}
+		
+		//send all remain packet
+		for( int i = 0; i < numSources; i++ ) {
+			while(this.pktCounter[i] < maxPacketNum)
+			{
+				if( uniformBool )
+					tmp = pkt.getUniformPacket(i);
+				else
+					tmp = pkt.getExponentialPacket(i);
+				try {
+					//enqueue tmp in the ith Lamport queue
+					this.lamportQueueBank[i].enq(tmp);
+					this.pktCounter[i]++;
+				} catch (FullException e) {;}
+			}
+		}
+	}
+}  
